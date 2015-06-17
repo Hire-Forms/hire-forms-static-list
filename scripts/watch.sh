912a2e37070e386e791aef60436c2ecb6108192b
Gijsjan <agijsbro@gmail.com>
2015-06-17 12:24:16 +0200

    Use Browserify instead of Watchify to be able to derequire

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-node_modules/.bin/watchify src/index.jsx \
+node_modules/.bin/browserify src/index.jsx \
   --detect-globals false \
   --extension=.jsx \
   --external classnames \
   --external react \
-  --outfile build/index.js \
   --standalone HireFormsStaticList \
   --transform [ babelify --plugins object-assign ] \
   --verbose | derequire > build/index.js
+
+# --outfile build/index.js \
